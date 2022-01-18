6262c1c7aec8857c37fb5c0c4627b5c529d9ad8f
Cornelius Müller <muellercornelius1@gmail.com>
2022-01-18 15:59:26 +0100

    changed to basic auth

--- _middleware.js
+++ _middleware.js
@@ -1,19 +1,20 @@
-//import { NextResponse } from 'next/server';
 export default function middleware(req) {
- console.log(req)
- /*  const basicAuth = req.headers.get('authorization')
+  const basicAuth = req.headers.get('authorization')
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
     if (user === '4dmin' && pwd === 'testpwd123') {
-      //return NextResponse.next()
-      return new Response('Auth required', {
-        status: 200,
-      })
+     console.log("WUHU")
+     // return NextResponse.next()
     }
-  } */
+  }
 
-  return new Response('Hello, world!');
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
 }
